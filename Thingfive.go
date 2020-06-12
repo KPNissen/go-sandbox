04017299606c275da56dbfe91dd100e4e0cc971a
Kellie <peevish6@gmail.com>
2020-06-12 10:20:46 +1000

    Update

--- Thingfive.go
+++ Thingfive.go
@@ -1,5 +1,17 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+)
 
+func Names() (string, string) {
+	return "Foo", "Bar"
+}
+
+func main() {
+	n1, n2 := Names()
+	fmt.Println(n1, n2)
+
+	n3, _ := Names()
+	fmt.Println(n3)
+}
